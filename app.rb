648d5afaf733620af7851c067458afe3de9fc65f
Ryan Graebner <ryan.graebner@gmail.com>
2012-10-16 17:31:12 -0700

    add link to db

--- app.rb
+++ app.rb
@@ -97,8 +97,17 @@ get "/new.html" do
 
     # for other data you can always run fql
     @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
+
+    @new=Mysql.new('us-cdbr-east.cleardb.com','a20b915a9b09e5','3dbe3bcc','heroku_6d2c5db5bc2c644')
+    @an_new=@new.query "INSERT INTO OR_TEST3 (fid,city,state,count,tool1,type1) VALUES('#{@user['id']}','Eugene','OR','1','#{[tool_1]}','#{[type_1]}')"
+    @new.close
   end
   erb :index
+end
+
+# used by Canvas apps - redirect the POST to be a regular GET
+post "/new.html" do
+  redirect "/new.html"
 end
 
 get "/friends_tools.html" do
