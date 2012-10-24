0f5e3fdc650137dd2132b124f5abfa8f944d8486
Ryan Graebner <ryan.graebner@gmail.com>
2012-10-23 17:45:44 -0700

    form setup

--- app.rb
+++ app.rb
@@ -100,6 +100,12 @@ get "/new.html" do
     # for other data you can always run fql
     @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
     
+    @m = Mysql.new('us-cdbr-east.cleardb.com','a20b915a9b09e5','3dbe3bcc','heroku_6d2c5db5bc2c644')
+    @an = @m.query "SELECT * FROM OR_TEST3 WHERE fid = '#{@user['id']}'"
+    @n_rows  = @an.num_rows
+    @inventory = @an.fetch_row.join("\s")
+    @m.close
+
     @query=CGI.new()
     @input=@query["tool_1"]
 
