22e9c281a1e07b0277b03648af341d31a3a66450
Ryan Graebner <ryan.graebner@gmail.com>
2012-10-24 19:27:23 -0700

    form help

--- cgi-bin/add_tool.rb
+++
@@ -1,8 +0,0 @@
-require "cgi"
-require "mysql"
-
-$query=CGI.new
-$new=Mysql.new('us-cdbr-east.cleardb.com','a20b915a9b09e5','3dbe3bcc','heroku_6d2c5db5bc2c644')
-$new.query "INSERT INTO OR_TEST3 (fid,city,state,count,tool1,type1) VALUES('700630645','Eugene','OR','1','#{@query[tool_1]}','Other')"
-$new.close
-
