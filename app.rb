fd0e0b8cc9e5f48731b6c810cab414dca8d465bb
runninggraeb <ryan.graebner@gmail.com>
2012-10-31 17:47:28 -0700

    updates

--- app.rb
+++ app.rb
@@ -84,32 +84,6 @@ post "/" do
   redirect "/"
 end
 
-get "/new.html" do
-  @graph  = Koala::Facebook::API.new(session[:access_token])
-
-  # Get public details of current application
-  @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
-
-  if session[:access_token]
-    @user    = @graph.get_object("me")
-    @friends = @graph.get_connections('me', 'friends')
-    @photos  = @graph.get_connections('me', 'photos')
-    @likes   = @graph.get_connections('me', 'likes').first(4)
-
-    # for other data you can always run fql
-    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
-
-
-  end
-  erb :new
-end
-
-# used by Canvas apps - redirect the POST to be a regular GET
-post "/new.html" do
-  @input = params[:tool_1]
-  redirect "/new.html"
-end
-
 get "/friends_tools.html" do
   # Get base API Connection
   @graph  = Koala::Facebook::API.new(session[:access_token])
@@ -161,6 +135,12 @@ end
 
 # used by Canvas apps - redirect the POST to be a regular GET
 post "/my_tools.html" do
+  @tool_1 = params[:tool_1]
+  @type_1 = params[:type_1]
+  @new=Mysql.new('us-cdbr-east.cleardb.com','a20b915a9b09e5','3dbe3bcc','heroku_6d2c5db5bc2c644')
+  @new.query "INSERT INTO OR_TEST3 (fid,city,state,count,tool1,type1) VALUES('#{@user['id']}','Eugene','OR','1','#{@tool_1}','#{@type_1}')"
+  @new.close
+
   redirect "/my_tools.html"
 end
 
