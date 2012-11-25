d150aa9206707cf120ee9f851b5b0574cfc01195
runninggraeb <ryan.graebner@gmail.com>
2012-11-25 11:38:17 -0800

    updates

--- app.rb
+++ app.rb
@@ -134,39 +134,31 @@ post "/my_tools.html" do
   @adds=0
   @news=Array.new(@count*2+11)
   @labels=Array.new(@count*2+11)
- # for i in 1..(@count+5)
- #   @temp=params[:"tool_#{@enter}"]
- #   if @temp
- #     @news[@adds*2+1] = "'#{@temp}'"
- #     @temp=params[:"type_#{@enter}"]
- #     @news[@adds*2+2] = "'#{@temp}'"
- #     @labels[@adds*2+1]="tool#{@enter}"
- #     @labels[@adds*2+2]="type#{@enter}"
- #     @adds +=1
- #   end
- #   @enter +=1
- # end
+  for i in 1..(@count+5)
+    @temp=params[:"tool_#{@enter}"]
+    if @temp
+      @news[@adds*2+1] = "'#{@temp}'"
+      @temp=params[:"type_#{@enter}"]
+      @news[@adds*2+2] = "'#{@temp}'"
+      @labels[@adds*2+1]="tool#{@enter}"
+      @labels[@adds*2+2]="type#{@enter}"
+      @adds +=1
+    end
+    @enter +=1
+  end
 
  #Try route matching
 
-  @adds=1
-  @temp=params[:"tool_#{@adds}"]
-  @news[0+1] = "'#{@temp}'"
-  @temp=params[:"type_#{@adds}"]
-  @news[0+2] = "'#{@temp}'"
-
-
-  @news=@news[1..2]
- # @news=@news[1..(@adds*2)]
- # @labels=@labels[1..(@adds*2)]
+  @news=@news[1..(@adds*2)]
+  @labels=@labels[1..(@adds*2)]
 
   @news=@news.join(',')
- # @labels=@labels.join(',')
+  @labels=@labels.join(',')
 
   @city = params[:city]
   @state = params[:state]
 
-  @new.query "INSERT INTO OR_TEST3 (fid,city,state,count,tool1,type1) VALUES('#{@user['id']}','#{@city}','#{@state}','#{@adds}',#{@news})"
+  @new.query "INSERT INTO OR_TEST3 (fid,city,state,count,#{@labels}) VALUES('#{@user['id']}','#{@city}','#{@state}','#{@adds}',#{@news})"
 
   @new.close
   redirect "/my_tools.html"
