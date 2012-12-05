3018ab243ef45f38badb3b7a23d8f28326458250
runninggraeb <ryan.graebner@gmail.com>
2012-12-05 15:19:43 -0800

    location fix

--- app.rb
+++ app.rb
@@ -205,6 +205,10 @@ post "/my_tools.html" do
   @new=Mysql.new('us-cdbr-east.cleardb.com','a20b915a9b09e5','3dbe3bcc','heroku_6d2c5db5bc2c644')
   @all = @new.query("SELECT * FROM Final_uni WHERE fid = '#{@user['id']}'").fetch_row
 
+  if @all == nil
+    @all=Arrar.new(5,0)
+  end
+
   @count = @all.at(4)
   @count = @count.to_i
 
