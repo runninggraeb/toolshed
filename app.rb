04fb44c5ac12bccfe9ebfa9039f457e7bec8acf1
runninggraeb <ryan.graebner@gmail.com>
2012-12-18 23:08:56 -0800

    friends fix

--- app.rb
+++ app.rb
@@ -103,9 +103,11 @@ get "/friends_tools.html" do
   @template[5]=Array.new(0)
 
   if @all !=nil
-    @state_list[0]=@all.at(3)
-    instance_variable_set(:"@#{@all.at(3)}", Array.new(@template))
-    @state_count +=1
+    if @all.at(2).length>1
+      @state_list[0]=@all.at(3)
+      instance_variable_set(:"@#{@all.at(3)}", Array.new(@template))
+      @state_count +=1
+    end
   end
 
   @it=0
