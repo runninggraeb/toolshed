595aff1d12dc4716eb10a829a17e22bb0f57d49a
runninggraeb <ryan.graebner@gmail.com>
2013-07-14 12:20:58 -0700

    fix mobile

--- app.rb
+++ app.rb
@@ -688,7 +688,7 @@ end
 get '/auth/facebook/callback' do
   session[:access_token] = authenticator.get_access_token(params[:code])
   if IsItMobile.mobile?(ENV["HTTP_USER_AGENT"])
-    redirect '/'
+    redirect 'https://toolshed.herokuapp.com/'
   else
     redirect 'https://apps.facebook.com/toolshed/'
   end
