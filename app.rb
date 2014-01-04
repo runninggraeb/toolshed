8aa58bfa010cfc47433343ec991cc521e210f44b
runninggraeb <ryan.graebner@gmail.com>
2014-01-03 17:39:31 -1000

    fix to mobile now?

--- app.rb
+++ app.rb
@@ -694,6 +694,10 @@ end
 #doesn't redirect mobile when not logged into facebook or app
 get '/auth/facebook/callback' do
   session[:access_token] = authenticator.get_access_token(params[:code])
-    redirect 'https://apps.facebook.com/toolshed/'
+    if IsItMobile.mobile?(ENV["HTTP_USER_AGENT"])
+      redirect 'https://toolshed.herokuapp.com/'
+    else
+      redirect 'https://apps.facebook.com/toolshed/'
+    end
 end
 
